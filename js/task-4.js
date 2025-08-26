2b39bde7a9817dfdb4277038d5578bd4359e2b55
Kristina-16-Kaguy <kristina.kaguy@gmail.com>
2025-08-26 21:48:18 +0300

    add task4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,19 @@
+const loginForm = document.querySelector(".login-form");
 
+loginForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  console.log(event);
+  const userName = event.target.elements.email.value.trim();
+  const userPassword = event.target.elements.password.value.trim();
+
+  if (userName === "" || userPassword === "") {
+    alert("All form fields must be filled in");
+  } else {
+    const payload = {
+      email: userName,
+      password: userPassword,
+    };
+    console.log(payload);
+    event.target.reset();
+  }
+});
